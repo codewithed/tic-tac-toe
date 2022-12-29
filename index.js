81e630a7ed6544957bea3b9debf4409fad910115
Edmund Debrah <edmundking05@gmail.com>
2022-12-29 17:56:11 +0000

    add player factory function and game factory function

--- index.js
+++ index.js
@@ -10,9 +10,29 @@ const GameBoard = (() => {
 
 // create Player factory function
 const Player = (playerName, playerType) => {
+  let played = false;
   const choose = (position) => {
-    if (GameBoard.board[position] === '') {
+    if (GameBoard.board[position] === '' && played === false) {
       GameBoard.board[position] = playerName;
+      played = true;
     }
   };
+  return {choose, played}
 };
+
+// create Game object
+const game = {
+  const start = (gameType) => {
+    let player1;
+    let player2;
+    if (gameType === 'pvp') {
+        player1 = Player('X','human');
+        player2 = Player('O','human');
+    }
+    else if (gameType === 'pvc') {
+        player1 = Player('X','human');
+        player2 = Player('O','bot');
+    }
+  }
+    const declareResult = ()
+}
