8f119e8476a4d36e039987d144ee863f56b56b6e
Edmund Debrah <edmundking05@gmail.com>
2022-12-31 21:22:34 +0000

    update script

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const GameBoard = (() => {
   // create board which is a 3X3 array
   const board = [
     ['', '', ''],
-    ['', '', ''],
+    ['', 'X', ''],
     ['', '', ''],
   ];
+  return { board };
 })();
 
+const boxes = document.querySelectorAll('#box');
+for (let x = 0; x < boxes.length; x += 1) {
+  for (let i = 0; i < 3; i += 1) {
+    for (let j = 0; j < 3; j += 1) {
+      boxes[x].textContent = GameBoard.board[i][j];
+    }
+  }
+}
+
+/*
 // create Player factory function
-const Player = (playerName, playerType) => {
+const player = (playerName, playerType) => {
   let played = false;
   const choose = (position) => {
     if (GameBoard.board[position] === '' && played === false) {
@@ -17,38 +28,39 @@ const Player = (playerName, playerType) => {
       played = true;
     }
   };
-  return {choose, played}
+  return { choose, played };
 };
 
 // create Game Module pattern
-const game = ({
-  const start = (gameType) => {
+const game = (() => {
+  const startGame = (gameType) => {
     let player1;
     let player2;
     if (gameType === 'pvp') {
-        player1 = Player('X','human');
-        player2 = Player('O','human');
+      player1 = player('X', 'human');
+      player2 = player('O', 'human');
+    } else if (gameType === 'pvc') {
+      player1 = player('X', 'human');
+      player2 = player('O', 'bot');
     }
-    else if (gameType === 'pvc') {
-        player1 = Player('X','human');
-        player2 = Player('O','bot');
-    }
-  }
-    const declareResult = () =>  {
-      // check for row
-      for(let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-          if (GameBoard.board[i,j] === GameBoard.board[i,j+3] === GameBoard.board[i,j+6] || GameBoard.board[i,j] === GameBoard.board[i,j+1] === GameBoard.board[i,j+2]){
+  };
 
-          }
+  const declareResult = () => {
+    // check for row
+    for (let i = 0; i < 3; i += 1) {
+      for (let j = 0; j < 3; j += 1) {
+        if (GameBoard.board[i][j] === GameBoard.board[i][j + 3] === GameBoard.board[i][j + 6] || GameBoard.board[i][j] === GameBoard.board[i][j + 1] === GameBoard.board[i][j + 2]) {
+
         }
-      }       
-    // check for diagonal 
-    if (GameBoard.board[0,0] === GameBoard.board[1,1] === GameBoard.board[2,2]) {
+      }
+    }
+    // check for diagonal
+    if (GameBoard.board[0][0] === GameBoard.board[1][1] === GameBoard.board[2][2]) {
       return true;
     }
-    if (GameBoard.board[0,2] === GameBoard.board[1,1] === GameBoard.board[2,0]) {
+    if (GameBoard.board[0][2] === GameBoard.board[1][1] === GameBoard.board[2][0]) {
       return true;
     }
-  }     
+  };
 })();
+*/
