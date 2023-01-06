c92f0518ebd10f7ddf5b2055894da25fbd4de07f
Edmund Debrah <edmundking05@gmail.com>
2023-01-06 14:19:42 +0000

    update checkForResult function

--- index.js
+++ index.js
@@ -46,16 +46,29 @@ const game = (() => {
     currentPlayer = playerX;
   };
 
+  const reStart = () => {
+    // reset gameboard
+    GameBoard.board = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ];
+
+    game.start();
+  };
+
   // add game event listeners to tic-tac-toe boxes
   const addListeners = () => {
     const box = document.querySelectorAll('#box');
     box.forEach((element) => {
       element.addEventListener('click', () => {
+        // selects chosen box
         const { index1 } = element.dataset;
         const { index2 } = element.dataset;
         if (GameBoard.board[index1][index2] === '') {
           GameBoard.board[index1][index2] = currentPlayer.name;
           displayController.updateScreen();
+          game.checkForResult();
           game.switchPlayer();
         }
       });
@@ -66,30 +79,44 @@ const game = (() => {
     if (currentPlayer === playerX) { currentPlayer = playerO; } else { currentPlayer = playerX; }
   };
 
-  const declareResult = () => {
+  const checkForResult = () => {
+    const result = document.getElementById('modal');
     // check for row
     for (let i = 0; i < 3; i += 1) {
       for (let j = 0; j < 3; j += 1) {
         if (GameBoard.board[i][j] === GameBoard.board[i][j + 1] === GameBoard.board[i][j + 2]) {
-          return true;
+          result.textContent = ` PLAYER ${GameBoard.board[i][j]} WINS`;
+          result.classList.add('active');
+          result.addEventListener('click', game.reStart);
         }
         if (GameBoard.board[i][j] === GameBoard.board[i][j + 3] === GameBoard.board[i][j + 6]) {
-          return true;
+          result.textContent = ` PLAYER ${GameBoard.board[i][j]} WINS`;
+          result.classList.add('active');
+          result.addEventListener('click', game.reStart);
         }
       }
     }
     // check for diagonal
     if (GameBoard.board[0][0] === GameBoard.board[1][1] === GameBoard.board[2][2]) {
-      return true;
+      result.textContent = ` PLAYER ${GameBoard.board[0][0]} WINS`;
+      result.classList.add('active');
+      result.addEventListener('click', game.reStart);
     }
     if (GameBoard.board[0][2] === GameBoard.board[1][1] === GameBoard.board[2][0]) {
-      return true;
+      result.textContent = ` PLAYER ${GameBoard.board[0][2]} WINS`;
+      result.classList.add('active');
+      result.addEventListener('click', game.reStart);
+    } else if (!GameBoard.board.includes('')) {
+      result.textContent = "IT'S A DRAW";
+      result.classList.add('active');
+      result.addEventListener('click', game.reStart);
     }
   };
   return {
-    start, switchPlayer, declareResult, addListeners,
+    start, switchPlayer, checkForResult, addListeners, reStart,
   };
 })();
 
+// initialize the game
 game.start();
 game.addListeners();
